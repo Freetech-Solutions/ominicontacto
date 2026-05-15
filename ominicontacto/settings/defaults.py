0646acc8c9fdb677b48482c16d6b290a61d11c9b
Isachi <isachiqba@gmail.com>
2026-05-15 14:04:58 +0200

    Mover SECRET_KEY de Django a variable de entorno

--- ominicontacto/settings/defaults.py
+++ ominicontacto/settings/defaults.py
@@ -37,8 +37,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 's1+*bfrvb@=k@c&9=pm!0sijjewneu5p5rojil#q+!a2y&as-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
