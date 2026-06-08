b0b1ac2e0648539594bb4e7aecbebc863bf33be9
fpignataro <fpignataro@MacBook-Pro-de-fabian.local>
2026-06-08 10:15:17 -0300

    acerca de

--- ominicontacto/settings/oml_settings_local.py
+++ ominicontacto/settings/oml_settings_local.py
@@ -38,8 +38,8 @@ GEARMAN_QUEUE_CALL = os.getenv('GEARMAN_QUEUE_CALL', 'acd-call-processor')
 if 'TOKEN_EXPIRED_AFTER_SECONDS' in os.environ:
     TOKEN_EXPIRED_AFTER_SECONDS = int(os.getenv('TOKEN_EXPIRED_AFTER_SECONDS'))
 # Settings para version de OML
-OML_BRANCH = os.getenv('OML_BUILD_DATE')
-OML_COMMIT = os.getenv('OML_BUILD_DATE')
+OML_BRANCH = os.getenv('OML_BRANCH')
+OML_COMMIT = os.getenv('OML_COMMIT')
 OML_BUILD_DATE = os.getenv('OML_BUILD_DATE')
 OML_PUBLIC_MEDIA_BASE_URL = os.getenv('OML_PUBLIC_MEDIA_BASE_URL')
 if 'OML_SIGNED_MEDIA_URL_EXPIRE_SECONDS' in os.environ:
